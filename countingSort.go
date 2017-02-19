4ac25e93c014c87e4b10a72825f7ca520621aa07
FuzzyStatic <allen.flickinger@gmail.com>
2017-02-19 11:54:45 -0500

    Counting sort

---
+++ countingSort.go
@@ -0,0 +1,51 @@
+/**
+* @Author: Allen Flickinger <FuzzyStatic>
+* @Date:   2017-02-18T12:28:32-05:00
+* @Email:  allen.flickinger@gmail.com
+* @Last modified by:   FuzzyStatic
+* @Last modified time: 2017-02-19T11:52:24-05:00
+ */
+
+package sort
+
+// CountingSort the frequencies of distinct elements of the array to be sorted is
+// counted and stored in an auxiliary array, by mapping its value as an index of the
+// auxiliary array.
+func (s *Sort) CountingSort() {
+
+	// First, find the maximum value in s.slice
+	k := 0
+
+	for _, v := range *s.slice {
+		if v > k {
+			k = v
+		}
+	}
+
+	sAux := make([]int, k+1) // Create sAux of size k + 1
+
+	// Initialize the elements of sAux with 0
+	for i := 0; i <= k; i++ {
+		sAux[i] = 0
+	}
+
+	// Store the frequencies of each distinct element of s.slice,
+	// by mapping its value as the index of sAux slice
+	for _, v := range *s.slice {
+		sAux[v] = sAux[v] + 1
+	}
+
+	sorted := make([]int, len(*s.slice))
+	j := 0
+
+	for i := 0; i <= k; i++ {
+		// Aux stores which element occurs how many times,
+		// Add i in sortedA[] according to the number of times i occured in A[]
+		for tmp := sAux[i]; tmp > 0; tmp-- {
+			sorted[j] = i
+			j++
+		}
+	}
+
+	*s.slice = sorted
+}
--- sort_test.go
+++ sort_test.go
@@ -3,7 +3,7 @@
 * @Date:   2017-02-18T12:08:00-05:00
 * @Email:  allen.flickinger@gmail.com
 * @Last modified by:   FuzzyStatic
-* @Last modified time: 2017-02-18T13:02:18-05:00
+* @Last modified time: 2017-02-19T11:53:37-05:00
  */
 
 package sort
@@ -13,10 +13,36 @@ import (
 	"testing"
 )
 
-var sorted = []int{1, 2, 3, 4, 5, 6, 7, 8, 9} // expected sort value
+var sorted = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}  // expected sort value
+var sorted2 = []int{1, 2, 4, 4, 5, 6, 7, 7, 8} // expected sort value
 
 func TestCountingSort(t *testing.T) {
+	slice1 := &[]int{9, 8, 7, 6, 5, 4, 3, 2, 1}
+	t.Logf("Test count sort of %v", *slice1)
+	s1, _ := New(slice1)
+	s1.CountingSort()
 
+	if !reflect.DeepEqual(*slice1, sorted) {
+		t.Errorf("Expected slice of %v, but it was %v instead.", sorted, *slice1)
+	}
+
+	slice2 := &[]int{6, 5, 4, 4, 8, 7, 7, 2, 1}
+	t.Logf("Test count sort of %v", *slice2)
+	s2, _ := New(slice2)
+	s2.CountingSort()
+
+	if !reflect.DeepEqual(*slice2, sorted2) {
+		t.Errorf("Expected slice of %v, but it was %v instead.", sorted2, *slice2)
+	}
+
+	slice3 := &[]int{9, 1, 8, 2, 7, 3, 6, 4, 5}
+	t.Logf("Test count sort of %v", *slice3)
+	s3, _ := New(slice3)
+	s3.CountingSort()
+
+	if !reflect.DeepEqual(*slice3, sorted) {
+		t.Errorf("Expected slice of %v, but it was %v instead.", sorted, *slice3)
+	}
 }
 
 func TestMergeSort(t *testing.T) {
@@ -29,13 +55,13 @@ func TestMergeSort(t *testing.T) {
 		t.Errorf("Expected slice of %v, but it was %v instead.", sorted, *slice1)
 	}
 
-	slice2 := &[]int{6, 5, 4, 9, 8, 7, 3, 2, 1}
+	slice2 := &[]int{6, 5, 4, 4, 8, 7, 7, 2, 1}
 	t.Logf("Test merge sort of %v", *slice2)
 	s2, _ := New(slice2)
 	s2.MergeSort(0, len(*slice2)-1) // Start at 0 and go to the last index value
 
-	if !reflect.DeepEqual(*slice2, sorted) {
-		t.Errorf("Expected slice of %v, but it was %v instead.", sorted, *slice2)
+	if !reflect.DeepEqual(*slice2, sorted2) {
+		t.Errorf("Expected slice of %v, but it was %v instead.", sorted2, *slice2)
 	}
 
 	slice3 := &[]int{9, 1, 8, 2, 7, 3, 6, 4, 5}
